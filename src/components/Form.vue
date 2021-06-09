57819e111c6f60594f4f5c1b3c9ab0aca820d16c
chaluvadi-jayanth <jayanth.chaluvadi@card91.io>
2021-06-09 12:57:51 +0530

    200-status

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -46,6 +46,7 @@
 <script>
 import { validationMixin } from "vuelidate";
 import { required, minLength, maxLength } from "vuelidate/lib/validators";
+import axios from "axios";
 export default {
   data() {
     return {
@@ -57,6 +58,33 @@ export default {
     };
   },
   methods: {
+    async postData(url = "") {
+      // Default options are marked with *
+      axios.create({
+        headers: {
+          "x-amz-acl": "public-read",
+          "Access-Control-Allow-Origin": "*",
+          //Authorization: 'Bearer {token}',
+          //timeout: 1000, // indicates, 1000ms ie. 1 second
+          "Content-Type": "application/json",
+        },
+      });
+      const response = await axios.post(
+        url
+        // method: "POST", // *GET, POST, PUT, DELETE, etc.
+        // mode: "no-cors", // no-cors, *cors, same-origin
+        // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        // credentials: "same-origin", // include, *same-origin, omit
+        // headers: {
+        //   "Content-Type": "application/json",
+        //   // 'Content-Type': 'application/x-www-form-urlencoded',
+        // },
+        // redirect: "follow", // manual, *follow, error
+        // referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        // // body: JSON.stringify(data), // body data type must match "Content-Type" header
+      );
+      return response.json(); // parses JSON response into native JavaScript objects
+    },
     formSubmit() {
       this.$v.$touch();
       this.submitted = true;
@@ -71,21 +99,36 @@ export default {
     },
     async googlerecaptcha() {
       await this.$recaptchaLoaded();
-      const token = await this.$recaptcha("save");
+      const token = await this.$recaptcha("login");
       console.log(token);
 
       if (token) {
+        // this.postData(
+        //   `https://www.google.com/recaptcha/api/siteverify?secret=6LfHRAwbAAAAABB3dlImlVHnHay9sWjMOYtWwrq0&response=${token}`
+        // ).then((data) => {
+        //   console.log(data); // JSON data parsed by `data.json()` call
+        // });
         fetch(
           `  https://www.google.com/recaptcha/api/siteverify?secret=6LfHRAwbAAAAABB3dlImlVHnHay9sWjMOYtWwrq0&response=${token} `,
           {
             mode: "no-cors",
             // credentials: "include",
             method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            headers: {
+              "Content-Type":
+                "application/x-www-form-urlencoded,application/json",
+            },
           }
         )
-          .then((res) => res.json())
-          .then((data) => console.log(data))
+          .then((res) => {
+            debugger;
+            return res.json();
+          })
+          .then((data) => {
+            debugger;
+            if (data) console.log(data);
+            else alert("no data");
+          })
           .catch((err) => console.log(err));
       }
     },
